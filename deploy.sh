d77745496059dce06a204c0c0b36179b2a705737
Aidan Do <aidand@semsci.com>
2023-07-30 22:05:24 +0930

    Deploy to AWS lambda and set for schedule

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+
+pyenv install 3.7
+pyenv local 3.7
+
+curl -SL https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip > chromedriver.zip
+curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-41/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+unzip chromedriver.zip
+unzip headless-chromium.zip
+zip chromdriver-v2.zip chromedriver headless-chromium
+
+python -m pip install -r requirements.txt -t python/lib/python3.7/site-packages
