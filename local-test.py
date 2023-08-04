6742f2ade0fd00368d3c994effb592dfc2a2b26d
Aidan Do <aidand@semsci.com>
2023-08-04 19:59:52 +0930

    Ability to test each job without invoking the ohters

--- local-test.py
+++ local-test.py
@@ -4,7 +4,14 @@ class FakeContext:
    def __init__(self) -> None:
       self.aws_request_id = "test-id"
 
-res = main(None, FakeContext())
+event = {
+   "disable_core_logic_crawl": True,
+   "force_run_proptrack_crawl": False,
+   "force_run_sqm_research_crawl": True,
+   "test_tweet": True,
+}
+
+res = main(event, FakeContext())
 
 print("Lambda response:")
 print(res)
